4157600360ebe1dd8d5222e0279a3c08c9a36a13
Karan Bhatt <186667142+13919201819@users.noreply.github.com>
2025-12-14 20:39:15 +0530

    removing comments

--- src/hooks/useIntersectionObserver.ts
+++ src/hooks/useIntersectionObserver.ts
@@ -1,23 +1,3 @@
-// import { useEffect, useRef, useState } from "react";
-
-// export function useIntersectionObserver() {
-//   const ref = useRef<any>();
-//   const [visible, setVisible] = useState(false);
-
-//   useEffect(() => {
-//     const obs = new IntersectionObserver(([entry]) =>
-//       setVisible(entry.isIntersecting)
-//     );
-
-//     if (ref.current) obs.observe(ref.current);
-
-//     return () => obs.disconnect();
-//   }, []);
-
-//   return { ref, visible };
-// }
-
-
 import { useEffect, useRef, useState } from "react";
 
 export function useIntersectionObserver() {
