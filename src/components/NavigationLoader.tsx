5df244d8d26886042ff5119be7ae4e465b7f94cf
Karan Bhatt <186667142+13919201819@users.noreply.github.com>
2026-01-17 16:23:58 +0530

    nav

--- src/components/NavigationLoader.tsx
+++ src/components/NavigationLoader.tsx
@@ -1,3 +1,22 @@
+// 'use client';
+
+// import { useEffect, useState } from 'react';
+// import { usePathname, useSearchParams } from 'next/navigation';
+// import Loader from '@/components/ui/Loader';
+
+// export default function NavigationLoader() {
+//   const pathname = usePathname();
+//   const searchParams = useSearchParams();
+//   const [loading, setLoading] = useState(false);
+
+//   useEffect(() => {
+//     setLoading(false);
+//   }, [pathname, searchParams]);
+
+//   return loading ? <Loader /> : null;
+// }
+
+
 'use client';
 
 import { useEffect, useState } from 'react';
@@ -11,7 +30,8 @@ export default function NavigationLoader() {
 
   useEffect(() => {
     setLoading(false);
-  }, [pathname, searchParams]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [pathname, searchParams.toString()]);
 
   return loading ? <Loader /> : null;
 }
